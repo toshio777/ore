bbc23501c71c8fe538f5564f0718b67922a961b1
toshio777 <629hitoshik@gmail.com>
2021-07-01 07:26:35 +0900

    changed

---
+++ db/migrate/20210630222013_create_horses.rb
@@ -0,0 +1,12 @@
+class CreateHorses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :horses do |t|
+      t.text :name
+      t.float :power
+      t.integer :year
+      t.integer :last_race
+
+      t.timestamps
+    end
+  end
+end
