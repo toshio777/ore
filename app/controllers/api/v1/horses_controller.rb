036b4a4a80fbe516950e19e257e30c822a50edee
toshio777 <629hitoshik@gmail.com>
2021-07-01 07:47:42 +0900

    changed

--- app/controllers/api/v1/horses_controller.rb
+++ app/controllers/api/v1/horses_controller.rb
@@ -4,12 +4,8 @@ module Api
       before_action :set_post, only: [:show, :update, :destroy]
 
       def index
-        horses = Horse.order(created_at: :desc)
+        horses = Horse.order(power: :desc)
         render json: { status: 'SUCCESS', message: 'Loaded horses', data: horses }
-      end
-
-      def show
-        render json: { status: 'SUCCESS', message: 'Loaded the post', data: @horse }
       end
 
       
