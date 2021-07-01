c68f95c8f305336094082480cfc014aec4eaabd6
toshio777 <629hitoshik@gmail.com>
2021-07-01 14:18:34 +0900

    changed

--- app/controllers/api/v1/horses_controller.rb
+++ app/controllers/api/v1/horses_controller.rb
@@ -1,8 +1,7 @@
 module Api
   module V1
     class HorsesController < ApplicationController
-      before_action :set_horse, only: [:show]
-
+      
       def index
         horses = Horse.order(power: :desc)
         render json: { data: horses }
