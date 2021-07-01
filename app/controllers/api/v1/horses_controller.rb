ab6dae4c2aa8383ea1f5a690df5e7e0535fa41ea
toshio777 <629hitoshik@gmail.com>
2021-07-01 14:58:10 +0900

    changed

--- app/controllers/api/v1/horses_controller.rb
+++ app/controllers/api/v1/horses_controller.rb
@@ -1,24 +1,46 @@
 module Api
   module V1
     class HorsesController < ApplicationController
-      before_action :set_horse, only: [:show]
-      
+      before_action :set_horse, only: [:show, :update, :destroy]
+
       def index
-        horses = Horse.order(power: :desc)
-        render json: { data: horses }
+        horses = Horse.order(created_at: :desc)
+        render json: { status: 'SUCCESS', message: 'Loaded posts', data: horses }
       end
 
       def show
-        render json: { data: @horse }
+        render json: { status: 'SUCCESS', message: 'Loaded the post', data: @horse }
+      end
+
+      def create
+        horse = Horse.new(post_params)
+        if horse.save
+          render json: { status: 'SUCCESS', data: horse }
+        else
+          render json: { status: 'ERROR', data: horse.errors }
+        end
+      end
+
+      def destroy
+        @horse.destroy
+        render json: { status: 'SUCCESS', message: 'Deleted the post', data: @horse }
+      end
+
+      def update
+        if @horse.update(post_params)
+          render json: { status: 'SUCCESS', message: 'Updated the post', data: @horse }
+        else
+          render json: { status: 'SUCCESS', message: 'Not updated', data: @horse.errors }
+        end
       end
 
       private
 
-      def set_horse
+      def set_post
         @horse = Horse.find(params[:id])
       end
-      
 
+      
     end
   end
 end
