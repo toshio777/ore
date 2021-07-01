51fe60e63eeab72dbd50436e600a0cc6597a5fee
toshio777 <629hitoshik@gmail.com>
2021-07-02 07:55:51 +0900

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
 
+      get "horses/random_choose" => "horses#random_choose"
       get "horses/:id" => "horses#show"
 
-      get "horses/random_choose" => "horses#random_choose"
-
+      
       resources :horses
-
-
-
     end
   end
 end
